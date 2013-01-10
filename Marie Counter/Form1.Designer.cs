83b428e08947905286a618a2391faf004bbf5f92
Matthew Hall <matthall547@gmail.com>
2013-01-09 23:41:12 -0800

    Added real files

---
+++ Marie Counter/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Marie_Counter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdUm = new System.Windows.Forms.Button();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmdUm
+            // 
+            this.cmdUm.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.cmdUm.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.cmdUm.FlatAppearance.BorderSize = 3;
+            this.cmdUm.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.cmdUm.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Yellow;
+            this.cmdUm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmdUm.Location = new System.Drawing.Point(74, 160);
+            this.cmdUm.Name = "cmdUm";
+            this.cmdUm.Size = new System.Drawing.Size(126, 60);
+            this.cmdUm.TabIndex = 0;
+            this.cmdUm.Text = "Um";
+            this.cmdUm.UseVisualStyleBackColor = false;
+            this.cmdUm.Click += new System.EventHandler(this.cmdUm_Click);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Font = new System.Drawing.Font("Lucida Fax", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCount.ForeColor = System.Drawing.Color.White;
+            this.lblCount.Location = new System.Drawing.Point(65, 55);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(0, 24);
+            this.lblCount.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.cmdUm);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdUm;
+        private System.Windows.Forms.Label lblCount;
+    }
+}
+
---
+++ Marie Counter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Marie Counter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Marie Counter")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c1a721cd-c456-498f-9797-619332984441")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
